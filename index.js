20708aedfdc39639c0b067b12180691abb61116b
sahil <47462984+sahil-repos@users.noreply.github.com>
2021-09-07 11:32:11 +0000

    Style: Added chalk messages

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //GENERIC QUIZ
 var que1 = {
-  que:"Name the longest River on earth?\n 1. Amazon \n 2. Missisipi \n 3.Nile \n enter the choice number only. ",
+  que:"Name the longest River on earth?\n 1. Amazon \n 2. Missisipi \n 3. Nile \n enter the choice number only. ",
   ans:"3"
   
 }
 var que2={
-  que:"Nearest sun to our solar system is ?\n 1. Proxima Centurai \n 2.Alpha Centurai  \n 3.Bernard's Star \n 4.Sirius \n enter the choice number only.",
+  que:"Nearest sun to our solar system is ?\n 1.Proxima Centurai \n 2.Alpha Centurai  \n 3.Bernard's Star \n 4.Sirius \n enter the choice number only.",
   ans:"1"
 }
 var que3={
@@ -15,11 +15,11 @@ var que3={
 
 
 var que4={
-  que:"Name the LARGEST River on earth?\n 1. Amazon \n 2. Missisipi \n 3.Nile \n enter the choice number only. ",
+  que:"Name the LARGEST River on earth?\n 1. Amazon \n 2. Missisipi \n 3. Nile \n enter the choice number only. ",
   ans:"1"
 }
 var que5={
-  que:"First Indian into space is \n 1.Kalpana Chawla \n 2.Rakesh Sharma \n 3.Sunita Williams \n enter choice number only ? ",
+  que:"First Indian into space is \n 1. Kalpana Chawla \n 2. Rakesh Sharma \n 3. Sunita Williams \n enter choice number only ? ",
   ans:"2"
 }
 
@@ -36,7 +36,7 @@ var que8={
   ans: "y"
 }
 var que9={
-  que:"What is the outermost region of the Sun’s atmosphere called? \n 1.Corona \n 2.Penumbra \n 3.Aurora ",
+  que:"What is the outermost region of the Sun’s atmosphere called? \n 1. Corona \n 2. Penumbra \n 3. Aurora ",
   ans:"1"
 }
 
@@ -57,7 +57,9 @@ var userScores = [
 ]
 
 function getScore() {
-   var userName = rls.question("Hi, let's play a Quiz \n Please enter your name to continue..");
+   var userName = rls.question("Hi, let's start our journey ! \n Please enter your name to continue..");
+    log(chalk.yellow.bold("\n Welcome to Level 1 !!!\n"));
+
   for (var i=0; i<queList.length-levelControl; i++) {
     var q = queList[i];
     testQuestion(q.que, q.ans)
